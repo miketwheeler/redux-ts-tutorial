958a20a508b56ba268b6bb3bdde2e481ceeaae02
miketwheeler <mikeytwheeler@gmail.com>
2023-01-14 12:13:35 -0800

    completed

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,17 +1,19 @@
-type Post = {
-    slug: string;
-    title: string;
+import { prisma } from "~/db.server";    
+import type { Post } from '@prisma/client';
+
+
+export async function getPosts() {
+    return prisma.post.findMany();
 }
 
-export async function getPosts(): Promise<Array<Post>> {
-    return [
-        {
-            slug: "my-first-post",
-            title: "My First Post",
+export async function getPost(slug: string) {
+    return prisma.post.findUnique({
+        where: {
+            slug,
         },
-        {
-            slug: "90s-mixtape",
-            title: "A 90s Mixtape I made just for you",
-        }
-    ]
+    });
+}
+
+export async function createPost(post: Pick<Post, "slug" | "title" | "markdown">) {
+    return prisma.post.create({ data: post });
 }
